b4307771f1a79500459784413ad09b351cc3dd8b
ALU-cn <riven1994@outlook.com>
2019-09-24 15:35:40 +0800

    可以批量替换文件名

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace AlterName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = Environment.CurrentDirectory;
+            string[] files = Directory.GetFiles(dir, "*.*", SearchOption.TopDirectoryOnly);
+            Console.WriteLine("当前目录为" + dir);
+            Console.WriteLine("找到" + files.Length + "个文件");
+            Console.WriteLine("请输入要替换的字符：");
+            string replaceNameInput = Console.ReadLine();
+            Console.WriteLine("将以上字符替换为：（或直接按回车删除以上字符）");
+            string newNameInput = Console.ReadLine();
+
+            int replacedNum = 0;
+            foreach (string str in files)
+            {
+                string currentName = Path.GetFileNameWithoutExtension(str);
+                if (currentName.Contains(replaceNameInput))
+                {
+                    string newName = currentName.Replace(replaceNameInput, newNameInput);
+                    File.Move(str, Path.GetDirectoryName(str) + "\\" + newName + Path.GetExtension(str));
+                    replacedNum++;
+                }
+                else
+                {
+                    Console.WriteLine(currentName + "这个文件名不包含" + newNameInput);
+                }
+
+            }
+
+            Console.WriteLine("修改完了" + replacedNum + "个，按任意键退出");
+            Console.ReadKey();
+        }
+    }
+}
